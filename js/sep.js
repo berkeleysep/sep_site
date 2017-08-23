951af9b4f02fb96df6fd4cbf58b02d7be6b61e31
Jacky Lu <jackylu@berkeley.edu>
2017-08-23 15:31:42 -0700

    clean up css/js

--- js/sep.js
+++ js/sep.js
@@ -1,12 +1,10 @@
 /**
+ * Much of this code is taken from Codrops, found here:
  * demo2.js
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2017, Codrops
- * http://www.codrops.com
  */
 {
 	// Helper vars and functions.
@@ -35,87 +33,11 @@
     return { x : posx, y : posy };
   };
 
-	// const TiltObj = function(el, options) {
-	// 	this.el = el;
-	// 	this.options = extend({}, this.options);
-	// 	extend(this.options, options);
-	// 	this.DOM = {};
-	// 	this.DOM.img = this.el.querySelector('.content__img');
-	// 	this.DOM.title = this.el.querySelector('.content__title');
-	// 	this._initEvents();
-	// }
-  //
-	// TiltObj.prototype.options = {
-	// 	movement: {
-	// 		img : { translation : {x: -40, y: -40} },
-	// 		title : { translation : {x: 20, y: 20} },
-	// 	}
-	// };
-  //
-	// TiltObj.prototype._initEvents = function() {
-	// 	this.mouseenterFn = (ev) => {
-	// 		anime.remove(this.DOM.img);
-	// 		anime.remove(this.DOM.title);
-	// 	};
-  //
-	// 	this.mousemoveFn = (ev) => {
-	// 		requestAnimationFrame(() => this._layout(ev));
-	// 	};
-  //
-	// 	this.mouseleaveFn = (ev) => {
-	// 		requestAnimationFrame(() => {
-	// 			anime({
-	// 				targets: [this.DOM.img, this.DOM.title],
-	// 				duration: 1500,
-	// 				easing: 'easeOutElastic',
-	// 				elasticity: 400,
-	// 				translateX: 0,
-	// 				translateY: 0
-	// 			});
-	// 		});
-	// 	};
-  //
-	// 	this.el.addEventListener('mousemove', this.mousemoveFn);
-	// 	this.el.addEventListener('mouseleave', this.mouseleaveFn);
-	// 	this.el.addEventListener('mouseenter', this.mouseenterFn);
-	// };
-  //
-	// TiltObj.prototype._layout = function(ev) {
-	// 	// Mouse position relative to the document.
-	// 	const mousepos = getMousePos(ev);
-	// 	// Document scrolls.
-	// 	const docScrolls = {left : document.body.scrollLeft + document.documentElement.scrollLeft, top : document.body.scrollTop + document.documentElement.scrollTop};
-	// 	const bounds = this.el.getBoundingClientRect();
-	// 	// Mouse position relative to the main element (this.DOM.el).
-	// 	const relmousepos = { x : mousepos.x - bounds.left - docScrolls.left, y : mousepos.y - bounds.top - docScrolls.top };
-  //
-	// 	// Movement settings for the animatable elements.
-	// 	const t = {
-	// 		img: this.options.movement.img.translation,
-	// 		title: this.options.movement.title.translation,
-	// 	};
-  //
-	// 	const transforms = {
-	// 		img : {
-	// 			x: (-1*t.img.x - t.img.x)/bounds.width*relmousepos.x + t.img.x,
-	// 			y: (-1*t.img.y - t.img.y)/bounds.height*relmousepos.y + t.img.y
-	// 		},
-	// 		title : {
-	// 			x: (-1*t.title.x - t.title.x)/bounds.width*relmousepos.x + t.title.x,
-	// 			y: (-1*t.title.y - t.title.y)/bounds.height*relmousepos.y + t.title.y
-	// 		}
-	// 	};
-	// 	this.DOM.img.style.WebkitTransform = this.DOM.img.style.transform = 'translateX(' + transforms.img.x + 'px) translateY(' + transforms.img.y + 'px)';
-	// 	this.DOM.title.style.WebkitTransform = this.DOM.title.style.transform = 'translateX(' + transforms.title.x + 'px) translateY(' + transforms.title.y + 'px)';
-	// };
-
   const DOM = {};
   DOM.svg = document.querySelector('.morph');
   DOM.shapeEl = DOM.svg.querySelector('path');
   DOM.contentElems = Array.from(document.querySelectorAll('.content-wrap'));
   console.log(DOM.contentElems);
-  // DOM.contentLinks = Array.from(document.querySelectorAll('.content__link'));
-  // DOM.footer = document.querySelector('.content--related');
   const contentElemsTotal = DOM.contentElems.length;
   const shapes = [
     {
@@ -315,45 +237,6 @@
           rotate: shapes[pos].rotate+'deg'
         });
       });
-      // watcher.exitViewport(function() {
-      //   const idx = !watcher.isAboveViewport ? pos-1 : pos+1;
-      //
-      //   if( idx <= contentElemsTotal && step !== idx ) {
-      //     console.log("exited viewport");
-      //     console.log(idx);
-      //     console.log(pos);
-      //     step = idx;
-      //     anime.remove(DOM.shapeEl);
-      //     anime({
-      //       targets: DOM.shapeEl,
-      //       duration: shapes[idx].animation.path.duration,
-      //       easing: shapes[idx].animation.path.easing,
-      //       elasticity: shapes[idx].animation.path.elasticity || 0,
-      //       d: shapes[idx].path,
-      //       fill: {
-      //         value: shapes[idx].fill.color,
-      //         duration: shapes[idx].fill.duration,
-      //         easing: shapes[idx].fill.easing
-      //       },
-      //       complete: function() {
-      //         initShapeLoop(idx);
-      //       }
-      //     });
-      //
-      //     anime.remove(DOM.svg);
-      //     anime({
-      //       targets: DOM.svg,
-      //       duration: shapes[idx].animation.svg.duration,
-      //       easing: shapes[idx].animation.svg.easing,
-      //       elasticity: shapes[idx].animation.svg.elasticity || 0,
-      //       scaleX: shapes[idx].scaleX,
-      //       scaleY: shapes[idx].scaleY,
-      //       translateX: shapes[idx].tx+'px',
-      //       translateY: shapes[idx].ty+'px',
-      //       rotate: shapes[idx].rotate+'deg'
-      //     });
-      //   }
-      // });
     });
   };
 
@@ -361,7 +244,6 @@
     imagesLoaded(document.body, () => {
       initShapeEl();
       createScrollWatchers();
-      // Array.from(document.querySelectorAll('.content--layout')).forEach(el => new TiltObj(el));
       document.body.classList.remove('loading');
     });
   };
