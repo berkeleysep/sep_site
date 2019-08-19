a6fea5e24ecefcb762baa0a3e1e5711ff0f64aab
Jennifer Lu <jennifer.z.lu@gmail.com>
2019-08-18 19:16:47 -0700

    Major refactoring to add node js

---
+++ app/server.js
@@ -0,0 +1,31 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/projects', function(req, res) {
+    res.sendFile(path.join(__dirname, 'projects.html'));
+});
+
+app.get('/rush', function(req, res) {
+    res.sendFile(path.join(__dirname, 'rush.html'));
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+}); 
